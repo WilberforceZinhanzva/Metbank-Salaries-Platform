96c63214cea2c73d136cec4221380c6efc98a7b1
Wilberforce <nimblecodezw@gmail.com>
2022-01-28 11:06:02 +0200

    cors config change

--- core-salaries-system/src/main/java/zw/co/metbank/coresalariessystem/security/ApplicationWebSecurity.java
+++ core-salaries-system/src/main/java/zw/co/metbank/coresalariessystem/security/ApplicationWebSecurity.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -55,7 +60,8 @@ public class ApplicationWebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .csrf().disable()
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers(SWAGGER_WHITELIST).permitAll()
                 .anyRequest()
@@ -83,6 +89,17 @@ public class ApplicationWebSecurity extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationFailureHandler authenticationFailureHandler(){
         return new CustomAuthenticationFailureHandler();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
--- core-salaries-system/src/main/java/zw/co/metbank/coresalariessystem/security/CorsConfig.java
+++ core-salaries-system/src/main/java/zw/co/metbank/coresalariessystem/security/CorsConfig.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Configuration
+
 public class CorsConfig implements Filter {
 
     public CorsConfig() {
